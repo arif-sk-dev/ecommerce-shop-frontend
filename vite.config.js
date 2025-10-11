3a61c2359d1d2f2211c4327375cc3cd6d7849ddb
arif-sk-dev <arifsk.dev@gmail.com>
2025-10-11 22:58:04 +0600

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ecommerce-shop-frontend/",
+  // base: "/ecommerce-shop-frontend/",
   plugins: [
     tailwindcss(),
     react()

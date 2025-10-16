08c8a566a9ca6afc0dc395038d2f787ab15f1bb9
arif-sk-dev <arifsk.dev@gmail.com>
2025-10-16 23:08:21 +0600

    initial commit

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -50,19 +50,18 @@ const Carousel = () => {
   }
 
   return (
-    <div className="container mt-15">
+    <div className="w-full overflow-hidden mt-15 pb-10">
       <Slider {...settings}>
         {data.slice(14, 20).map((item, index) => {
           return (
             <div
               key={index}
-              // className="container bg-gradient-to-r from-[#006e6e] via-[#00a896] to-[#40e0d0] py-4 w-screen md:h-[70vh] lg:h-[70vh] h-[40vh]">
-              className="container bg-zinc-200 py-4 w-screen md:h-[70vh] lg:h-[70vh] h-[35vh] z-10">
-                <div className="flex flex-cols-4 gap-10 justify-between mx-auto  items-center px-4">
+              className="w-full bg-zinc-200 py-4 h-[35vh] md:h-[70vh] lg:h-[70vh] z-10">
+                <div className="container flex flex-cols-4 gap-10 justify-between mx-auto  items-center px-4">
                   <div className="space-y-6 flex-3 items-center">
                     <h3 className="bg-gradient-to-r from-[#470165] via-[#049385] to-[#880e05]     bg-clip-text text-transparent lg:text-4xl md:text-2xl sm:text-[16px] font-bold mx-auto">Brighten Your World With The Best Fashion!</h3>
                     <h1 className="bg-gradient-to-r from-[#030caf] via-[#940404] to-[#02681b]     bg-clip-text text-transparent md:text-4xl sm:text-2xl font-bold mx-auto">{item.title}</h1>
-                    {/* <h1 className="container sm:hidden md:block font-semibold text-zinc-800">{item.description}</h1> */}
+
                     <div>
                       <button className="bg-gradient-to-r from-[#006e6e] via-[#00a896] to-[#ab0c0c] text-white border-1 px-3 py-2 rounded-[5px] cursor-pointer">Shop Now</button>
                     </div>

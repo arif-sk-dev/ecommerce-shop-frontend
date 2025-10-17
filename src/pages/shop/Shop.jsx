1394a305d29ed9f1856c9dc919ae4ab1cbc60c4f
arif-sk-dev <arifsk.dev@gmail.com>
2025-10-17 14:33:54 +0600

    initial commit

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getData } from './../../context/DataContext';
+// import FilterSection from '../../components/FilterSection';
+import Loading from '../../../src/assets/Loading.gif'
 
 const Shop = () => {
+  const {data, fetchAllProducts} =getData();
+
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
   return (
-    <div>
-      <h2>Shop page</h2>
-    </div>
+    <section>
+      <div className='max-w-6xl mx-auto px-4 pb-10 mt-20'>
+        {
+          data?.length > 0 ? (
+            <div className='flex gap-10'>
+
+              {/* Filter Section  */}
+              <FilterSection />
+
+              {/* Product Section  */}
+              <div>
+
+              </div>
+            </div>
+          ) : (
+            <div className='text-2xl text-zinc-500 italic text-center pt-10 h-screen'>
+              <img src={Loading} alt="Loading..." className="mx-auto w-20 h-20" />
+              <p>Loading products...</p>
+            </div>
+          )
+        }
+      </div>
+    </section>
   );
 };
 
